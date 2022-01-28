e4b0f60359539765abaf611e0735f625c0a09522
Eduard Ragea <eduard.ragea@kcl.ac.uk>
2022-01-28 16:40:21 +0000

    Create folders for file organisation. Move map code to a different screen.

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/bitmap.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/location.dart';
+
+const LatLng SOURCE_LOCATION = LatLng(51.51185004458236, -0.11580820118980878); //points to bush house - CHANGE this to users current live location
+const LatLng DEST_LOCATION = LatLng(42.744421,-71.1698939); //dummy value for time being - change DEST_LOCATION to what the user inputs later on
+const double CAMERA_ZOOM = 16;
+const double CAMERA_TILT = 80;
+const double CAMERA_BEARING = 30;
+
+void main() {
+  runApp(MapPage());
+}
+
+class MapPage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState(){
+    return _MyHomePageState();
+  } 
+}
+
+class _MyHomePageState extends State<MapPage>{
+
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _markers = Set<Marker>();
+  late LatLng currentLocation;
+  late LatLng destinationLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    //set up inital locations
+    this.setInitialLocation();
+  }
+
+  void setInitialLocation(){
+    currentLocation = LatLng(
+      SOURCE_LOCATION.latitude,
+      SOURCE_LOCATION.longitude
+    );
+
+    destinationLocation = LatLng(
+      DEST_LOCATION.latitude,
+      DEST_LOCATION.longitude
+    );
+  }
+
+  @override 
+  Widget build(BuildContext context){
+
+    CameraPosition _initialCameraPosition = CameraPosition(
+      zoom: CAMERA_ZOOM,
+      tilt: CAMERA_TILT,
+      bearing: CAMERA_BEARING,
+      target: SOURCE_LOCATION
+    );
+
+      return MaterialApp(
+        home: Stack(
+          children: <Widget> [
+            Positioned.fill(
+              child: GoogleMap(
+              myLocationEnabled: false,
+              compassEnabled: false,
+              tiltGesturesEnabled: false,
+              markers: _markers,
+              mapType: MapType.normal,
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
