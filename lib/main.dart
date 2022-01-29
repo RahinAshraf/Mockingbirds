44648e27db23fc5ce035aa6764523464979cf53a
Eduard Ragea <eduard.ragea@kcl.ac.uk>
2022-01-29 16:35:22 +0000

    Add Splash Screen and autologin to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'navbar.dart';
+import 'package:provider/provider.dart';
+
+import './screens/map_screen.dart';
+import './screens/auth_screen.dart';
+import './providers/auth.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+              title: 'Veloplan',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.green,
+                ).copyWith(
+                  secondary: Colors.black,
+                ),
+              ),
+              //home: const MyHomePage(title: 'Topper'),
+              home: auth.isAuth ? MapPage() : AuthScreen(),
+              routes: {
+                '/map': (ctx) => MapPage(),
+                '/auth' : (ctx) => const AuthScreen(),
+              },
+              onGenerateRoute: (settings) {
+                //print(settings.arguments);
+                return MaterialPageRoute(builder: (ctx) => AuthScreen());
+              },
+              onUnknownRoute: (settings) {
+                return MaterialPageRoute(builder: (ctx) => AuthScreen());
+              },
+            ),
+      ),
+    );
+  }
 }
