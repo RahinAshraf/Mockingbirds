d92b4f4a89ba66c116257dbc31ca171bac1881d2
FarihaChoudhury <fariha.choudhury@kcl.ac.uk>
2022-02-02 15:57:39 +0000

    Add email and age to profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../model/user_preference.dart';
+import '../model/user.dart';
 import '../widget/profile_widget.dart';
+//import 'package:user_profile_example/widget/button_widget.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -21,9 +23,36 @@ class _ProfileState extends State<Profile> {
           ProfileWidget(
             imagePath: user.imagePath,
             onClicked: () async {},
-          )
+          ),
+          const SizedBox(height: 24),
+          buildName(user),
         ],
       ),
     );
   }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.email,
+            style: TextStyle(color: Colors.grey),
+          ),
+          Text(
+            user.age,
+            style: TextStyle(color: Colors.grey),
+          ),
+          // const SizedBox(height: 24),
+          // Center(child: buildStatisticsButton()),
+        ],
+      );
 }
+
+// Widget buildStatisticsButton() => ButtonWidget(
+//       text: 'Statistics',
+//       onClicked: () {},
+//     );
