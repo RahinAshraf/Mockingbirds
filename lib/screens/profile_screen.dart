e34a892f406ef0da43e69de5af00df229099657f
tayy-b <k20059556@kcl.ac.uk>
2022-02-02 14:50:59 +0000

    Add basic profile pictire and create user model

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import '../model/user_preference.dart';
+import '../widget/profile_widget.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text('My Profile'),
       ),
-      body: Center(
-        child: Text("Profile page!"),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          )
+        ],
       ),
     );
   }
---
+++ lib/widget/profile_widget.dart
@@ -0,0 +1,40 @@
+//import 'dart:html';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class ProfileWidget extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onClicked;
+
+  const ProfileWidget({
+    Key? key,
+    required this.imagePath,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+
+    return Center(
+      child: buildImage(),
+    );
+  }
+
+  Widget buildImage() {
+    final image = NetworkImage(imagePath);
+
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent,
+        child: Ink.image(
+          image: image,
+          fit: BoxFit.cover,
+          width: 128,
+          height: 128,
+          child: InkWell(onTap: onClicked),
+        ),
+      ),
+    );
+  }
+}
