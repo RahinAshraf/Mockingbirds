9d00e3f23791e49fff0c895378a6d686499cb9cd
tayy-b <k20059556@kcl.ac.uk>
2022-02-10 19:47:08 +0000

    Replace body of scaffold with a stack in build() map_screen.dart

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,60 +4,145 @@ import '../navbar.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latLng;
 import '../.env.dart';
+import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   @override
   MyHomePageState createState() => MyHomePageState();
 }
 
+// class MyHomePageState extends State<MapScreen> {
+//   Location _currentLocation = Location();
+
+//   @override
+//   Widget build(BuildContext build) {
+//     return Scaffold(
+//       body: FlutterMap(
+//         options: MapOptions(
+//           center: latLng.LatLng(51.51185004458236, -0.11580820118980878),
+//           zoom: 16.0,
+//         ),
+//         layers: [
+//           TileLayerOptions(
+//             urlTemplate:
+//                 "https://api.mapbox.com/styles/v1/mockingbirds/ckzh4k81i000n16lcev9vknm5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNremd3NW9weDM2ZmEybm45dzlhYzN0ZnUifQ.lSzpNOhK2CH9-PODR0ojLg",
+//             additionalOptions: {
+//               'accessToken': MAPBOX_ACCESS_TOKEN,
+//               'id': 'mapbox.mapbox-streets-v8'
+//             },
+//           ),
+//           MarkerLayerOptions(
+//             markers: [
+//               Marker(
+//                   point: latLng.LatLng(51.51185004458236, -0.11580820118980878),
+//                   builder: (_) {
+//                     return Container(
+//                       height: 50,
+//                       width: 50,
+//                       decoration: BoxDecoration(
+//                         color: Colors.red[300],
+//                         shape: BoxShape.circle,
+//                       ),
+//                     );
+//                   }),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 class MyHomePageState extends State<MapScreen> {
+  Location _currentLocation = Location();
+
   @override
   Widget build(BuildContext build) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          center: latLng.LatLng(51.51185004458236, -0.11580820118980878),
-          zoom: 16.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate:
-                "https://api.mapbox.com/styles/v1/mockingbirds/ckzh4k81i000n16lcev9vknm5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNremd3NW9weDM2ZmEybm45dzlhYzN0ZnUifQ.lSzpNOhK2CH9-PODR0ojLg",
-            additionalOptions: {
-              'accessToken': MAPBOX_ACCESS_TOKEN,
-              'id': 'mapbox.mapbox-streets-v8'
-            },
-            attributionBuilder: (_) {
-              return Text("VeloPlan");
-            },
-          ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                  point: latLng.LatLng(51.51185004458236, -0.11580820118980878),
-                  builder: (_) {
-                    return Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.red[300],
-                        shape: BoxShape.circle,
-                      ),
-                    );
-                  }),
+        body: Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: FlutterMap(
+            options: MapOptions(
+              center: latLng.LatLng(51.51185004458236, -0.11580820118980878),
+              zoom: 16.0,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate:
+                    "https://api.mapbox.com/styles/v1/mockingbirds/ckzh4k81i000n16lcev9vknm5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNremd3NW9weDM2ZmEybm45dzlhYzN0ZnUifQ.lSzpNOhK2CH9-PODR0ojLg",
+                additionalOptions: {
+                  'accessToken': MAPBOX_ACCESS_TOKEN,
+                  'id': 'mapbox.mapbox-streets-v8'
+                },
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  Marker(
+                      point: latLng.LatLng(
+                          51.51185004458236, -0.11580820118980878),
+                      builder: (_) {
+                        return Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.red[300],
+                            shape: BoxShape.circle,
+                          ),
+                        );
+                      }),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ));
   }
 }
 
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
 
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
 
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition();
+}
 
 //this is the old google maps stuff:
 
+// import 'dart:async';
+// import 'dart:math';
+// // import 'dart:html';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:google_maps_flutter_platform_interface/src/types/location.dart';
+// import 'package:google_maps_routes/google_maps_routes.dart';
+// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+// import 'package:location/location.dart';
+// // import 'package:latlong/latlong.dart' as ll;
+// import 'package:veloplan/screens/location_service.dart';
 
 // // ! remove
 // const LatLng SOURCE_LOCATION = LatLng(51.51185004458236,
@@ -310,16 +395,3 @@ class MyHomePageState extends State<MapScreen> {
 //     return points;
 //   }
 // }
-
-// import 'dart:async';
-// import 'dart:math';
-// // import 'dart:html';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:google_maps_flutter_platform_interface/src/types/location.dart';
-// import 'package:google_maps_routes/google_maps_routes.dart';
-// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-// import 'package:location/location.dart';
-// // import 'package:latlong/latlong.dart' as ll;
-// import 'package:veloplan/screens/location_service.dart';
