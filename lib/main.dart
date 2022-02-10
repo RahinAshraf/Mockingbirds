4f2a2fb0f959f4f9c126f03b9503e111ac0059c3
tayy-b <k20059556@kcl.ac.uk>
2022-02-10 17:49:01 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'navbar.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
+
+// void main() {
+//   runApp(MaterialApp(
+//     initialRoute: '/',
+//     routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
+//   ));
+// }
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext build) {
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: latLng.LatLng(51.5, -0.09),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            additionalOptions: {
+              'accessToken':
+                  'pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNremd3NW9weDM2ZmEybm45dzlhYzN0ZnUifQ.lSzpNOhK2CH9-PODR0ojLg',
+              'id': 'mapbox.mapbox-streets-v8'
+            },
+            attributionBuilder: (_) {
+              return Text("© OpenStreetMap contributors");
+            },
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: latLng.LatLng(51.5, -0.09),
+                builder: (ctx) => Container(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
