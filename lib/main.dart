6dfa084289e639645fe1d24a384f335a4ab46b94
tayy-b <k20059556@kcl.ac.uk>
2022-02-12 14:32:07 +0000

    Set up navigation API

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'navbar.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
-  ));
-}
+// void main() {
+//   runApp(MaterialApp(
+//     initialRoute: '/',
+//     routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
+//   ));
+// }
+
+import 'package:flutter/material.dart';
+
+import 'navigation.dart';
+
+void main() => runApp(SampleNavigationApp());
