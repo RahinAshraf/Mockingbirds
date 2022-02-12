6bf9d5e96036decb602c98a3058978505c6c632f
FarihaChoudhury <fariha.choudhury@kcl.ac.uk>
2022-02-12 23:36:58 +0000

    Fix mismatched public access tokens for MapBox

--- lib/.env.dart
+++ lib/.env.dart
@@ -1,2 +1,2 @@
 const MAPBOX_ACCESS_TOKEN =
-    'pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNremd3NW9weDM2ZmEybm45dzlhYzN0ZnUifQ.lSzpNOhK2CH9-PODR0ojLg';
+    'pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNrempyNnZtajNkbmkybm8xb3lybWE3MTIifQ.AsZJbQPNRb2N3unNdA98nQ';
