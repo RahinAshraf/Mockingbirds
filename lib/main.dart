d68c08a55e0046f25021fae4cc9bb1ff4b7b6338
Hristina-Andreea Sararu <hristina-andreea.sararu@kcl.ac.uk>
2022-02-14 00:36:44 +0000

    improve splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,31 @@ import 'navbar.dart';
 
 void main() {
   runApp(MaterialApp(
+<<<<<<< Updated upstream
     // ThemeData(primarySwatch: Colors.lime),
     initialRoute: '/',
     routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
   ));
+=======
+      initialRoute: '/',
+      routes: {'/': (context) => Login(), '/map': (context) => Navbar()},
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xffffffff),
+        primarySwatch: const MaterialColor(
+          0xff99d2a9, // 0%
+          const <int, Color>{
+            50: const Color(0xffa3d7b2), //10%
+            100: const Color(0xffaddbba), //20%
+            200: const Color(0xffb8e0c3), //30%
+            300: const Color(0xffc2e4cb), //40%
+            400: const Color(0xffcce9d4), //50%
+            500: const Color(0xffd6eddd), //60%
+            600: const Color(0xffe0f2e5), //70%
+            700: const Color(0xffebf6ee), //80%
+            800: const Color(0xfff5fbf6), //90%
+            900: const Color(0xffffff), //100%
+          },
+        ),
+      )));
+>>>>>>> Stashed changes
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,11 +3,96 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return Scaffold(
         appBar: AppBar(
           title: const Text('LogIn'),
         ),
         body: MyStatefulWidget());
+=======
+    return Scaffold(body: MyStatefulWidget());
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Container(
+            height: 190.0,
+            width: 190.0,
+            alignment: Alignment.topLeft,
+            child: Image.asset('assets/images/bubbles-shapes.png')),
+        Container(
+            height: 190.0,
+            width: 190.0,
+            child:
+                Center(child: Image.asset('assets/images/woman-cycling.png'))),
+        Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Cycle the city',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25),
+            )),
+        Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Plan with us your journeys through London!',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            )),
+        Container(
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(75, 5, 75, 5),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 10.0,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15.0),
+                ),
+              ),
+              child:
+                  const Text('Log in', style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            )),
+        Container(
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(75, 5, 75, 5),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 10.0,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15.0),
+                ),
+              ),
+              child:
+                  const Text('Sign Up', style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            )),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/map');
+          },
+          child: const Text('Go to map!'),
+        ),
+      ],
+    );
+>>>>>>> Stashed changes
   }
 }
 
