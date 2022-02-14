39d55f8f3e3beda73e4b4f90b858ea68c5826e04
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2022-02-14 17:19:30 +0000

    Create sort by distance functionality
    - Create helpers folder
    - Create navigation_helpers.dart file
    - Create sortByDistance method

---
+++ lib/helpers/navigation_helpers.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+import 'package:latlong2/latlong.dart';
+
+double calculateDistance(LatLng pos1, LatLng pos2) {
+  var p = 0.017453292519943295;
+  var a = 0.5 -
+      cos((pos2.latitude - pos1.latitude) * p) / 2 +
+      cos(pos1.latitude * p) *
+          cos(pos2.latitude * p) *
+          (1 - cos((pos2.longitude - pos1.longitude) * p)) /
+          2;
+  return 12742 * asin(sqrt(a));
+}
+
+List<LatLng> sortByDistance(LatLng start, List<LatLng> points) {
+  points.sort((a, b) => calculateDistance(start, a)
+      .compareTo(calculateDistance(start, b).toDouble()));
+  return points;
+}
