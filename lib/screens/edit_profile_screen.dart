43d06ae7ab77c9e5e72cb19e5e122737956e81f3
Eduard Ragea <eduard.ragea@kcl.ac.uk>
2022-02-25 18:47:45 +0000

    Check if username already exists.

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../widget/profile_widget.dart';
@@ -6,7 +7,7 @@ import '../widget/textfield_widget.dart';
 //import 'package:user_profile_example/widget/button_widget.dart';
 
 class EditProfile extends StatefulWidget {
-  final Map<String,dynamic> data;
+  final Map<String, dynamic> data;
   const EditProfile(this.data, {Key? key}) : super(key: key);
 
   @override
@@ -14,13 +15,47 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
-  final user = FirebaseAuth.instance.currentUser!.uid;
+  final userID = FirebaseAuth.instance.currentUser!.uid;
   var _firstName = '';
   var _lastName = '';
   var _username = '';
 
-  void _submitChanges() {
+  Future<bool> _checkUsernameIsFree() async {
+    if (_username == widget.data['username']) {
+      return true;
+    }
+    print(_username);
+    return (await FirebaseFirestore.instance
+                .collection('users')
+                .where('username', isEqualTo: _username)
+                .get())
+            .docs.isEmpty;
+  }
 
+  void _submitChanges() {
+    try {
+      _checkUsernameIsFree().then((value) {
+        if (!value) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text(
+                  'This username is already taken! Please choose something else.'),
+              backgroundColor: Theme.of(context).errorColor,
+            ),
+          );
+          return;
+        } else {
+          Navigator.of(context).pop();
+        }
+      });
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.toString()),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    }
   }
 
   @override
@@ -32,19 +67,18 @@ class _EditProfileState extends State<EditProfile> {
           title: const Text('Edit profile'),
           actions: [
             Padding(
-          padding: const EdgeInsets.only(right: 20.0),
-          child: GestureDetector(
-            onTap: () {
-              _submitChanges();
-              Navigator.of(context).pop();
-            },
-            child: const Icon(
-              Icons.check,
-              size: 26.0,
-              color: Colors.green,
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  _submitChanges();
+                },
+                child: const Icon(
+                  Icons.check,
+                  size: 26.0,
+                  color: Colors.green,
+                ),
+              ),
             ),
-          ),
-        ),
           ],
         ),
         backgroundColor: Colors.white,
@@ -52,8 +86,7 @@ class _EditProfileState extends State<EditProfile> {
           padding: EdgeInsets.symmetric(horizontal: 32),
           physics: BouncingScrollPhysics(),
           children: [
-            ProfileWidget(widget.data['image_url'], () async {}, true 
-            ),
+            ProfileWidget(widget.data['image_url'], () async {}, true),
             const SizedBox(height: 24),
             TextFieldWidget(
               label: 'First Name',
