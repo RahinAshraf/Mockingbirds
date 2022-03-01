885e1c8e2a35c18b7cd3cdae5468d9c19463174a
Hristina-Andreea Sararu <hristina-andreea.sararu@kcl.ac.uk>
2022-03-01 13:50:41 +0000

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'navbar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:location/location.dart';
+import 'package:veloplan/main.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import './screens/verify_email_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+late SharedPreferences sharedPreferences;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
+  initializeLocation(); //Upon opening the app, store the users current location
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {'/': (context) => const MyApp(), '/map': (context) => Navbar()},
+  ));
+}
+
+void initializeLocation() async {
+  Location _location = Location();
+  bool? _serviceEnabled;
+  PermissionStatus? _permissionGranted;
+
+  _serviceEnabled = await _location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await _location.requestService();
+  }
+
+  _permissionGranted = await _location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await _location.requestPermission();
+  }
+
+  LocationData _locationData = await _location.getLocation();
+  LatLng currentLatLng =
+      LatLng(_locationData.latitude!, _locationData.longitude!);
+
+  saveLocation(_locationData);
+}
+
+void saveLocation(LocationData _locationData) {
+  sharedPreferences.setDouble('latitude', _locationData.latitude!);
+  sharedPreferences.setDouble('longitude', _locationData.longitude!);
 }
 
 class MyApp extends StatefulWidget {
