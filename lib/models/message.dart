f415025c19a6587bf4122e41b224e0884a95874a
Lilianna Szabo <szabo.lilianna@gmail.com>
2022-03-07 10:43:48 +0000

    Pressing other question launches mail app in helpbot

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -2,6 +2,12 @@ class Message {
   final String questionText;
   final String questionAnswer;
   final String topic;
+  bool launch = false;
 
   Message(this.questionText, this.questionAnswer, this.topic);
+
+
+  void setLaunch(bool launch){
+    this.launch = launch;
+  }
 }
