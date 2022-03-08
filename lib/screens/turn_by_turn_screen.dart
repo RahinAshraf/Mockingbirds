fde4aee465e39f44dd52bce4ab37af869cdf969a
Fariha Choudhury <fariha.choudhury@kcl.ac.uk>
2022-03-08 12:07:02 +0000

    Change mapbox tokens

---
+++ lib/screens/turn_by_turn_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/library.dart';
+import 'package:intl/number_symbols_data.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:veloplan/screens/map_screen.dart';
+import 'package:veloplan/helpers/latlng_to_waypoint.dart';
+import 'dart:async';
+
+//Reference: dormmom.com, Jul 20, 2021, flutter_mapbox_navigation 0.0.26, https://pub.dev/packages/flutter_mapbox_navigation
+
+class TurnByTurn extends StatefulWidget {
+  late List<LatLng> points;
+  TurnByTurn(List<LatLng> points) {
+    this.points = points;
+  }
+  @override
+  State<TurnByTurn> createState() => _TurnByTurnState(points);
+}
+
+class _TurnByTurnState extends State<TurnByTurn> {
+  late List<LatLng> points;
+  late var wayPoints = <WayPoint>[];
+
+  _TurnByTurnState(List<LatLng> points) {
+    this.points = points;
+    wayPoints = latLngs2WayPoints(points);
+  }
+
+  // Config variables for Mapbox Navigation
+  late MapBoxNavigation directions;
+  late MapBoxOptions _options;
+  late double distanceRemaining, durationRemaining;
+  late MapBoxNavigationViewController _controller;
+  final bool isMultipleStop = true;
+  String instruction = "";
+  bool arrived = false;
+  bool routeBuilt = false;
+  bool isNavigating = false;
+  late DateTime startTime;
+  
+  //late Timer timer;
+  //String hello = "hello";
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    if (!mounted) return;
+
+    startTime = DateTime.now();
+
+    // Setup directions and options
+    directions = MapBoxNavigation(onRouteEvent: _onRouteEvent);
+    _options = MapBoxOptions(
+        zoom: 18.0,
+        voiceInstructionsEnabled: true,
+        bannerInstructionsEnabled: true,
+        mode: MapBoxNavigationMode.cycling,
+        isOptimized: true,
+        units: VoiceUnits.metric,
+        simulateRoute: true, //false to use live movement
+        language: "en");
+
+    // Start the trip
+    await directions.startNavigation(wayPoints: wayPoints, options: _options);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MapPage();
+  }
+
+  Future<void> _onRouteEvent(e) async {
+    DateTime currentTime = DateTime.now();
+    /// create timer 
+    distanceRemaining = await directions.distanceRemaining;
+    durationRemaining = await directions.durationRemaining;
+
+    switch (e.eventType) {
+      case MapBoxEvent.progress_change:
+        var progressEvent = e.data as RouteProgressEvent;
+        arrived = progressEvent.arrived!;
+        if (progressEvent.currentStepInstruction != null) {
+          instruction = progressEvent.currentStepInstruction!;
+        }
+        break;
+      case MapBoxEvent.route_building:
+      case MapBoxEvent.route_built:
+        routeBuilt = true;
+        break;
+      case MapBoxEvent.route_build_failed:
+        routeBuilt = false;
+        break;
+      case MapBoxEvent.navigation_running:
+        isNavigating = true;
+        break;
+      case MapBoxEvent.on_arrival:
+        arrived = true;
+        if (!isMultipleStop) {
+          await Future.delayed(const Duration(seconds: 3));
+          await _controller.finishNavigation();
+        } else {}
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        routeBuilt = false;
+        isNavigating = false;
+        break;
+      default:
+        break;
+    }
+    //refresh UI
+    setState(() {
+      //changeStuff();
+      // if (startTime.AddMinutes(20) = currentTime){
+      //   print("5 mins");
+      // }
+      if (startTime.compareTo(currentTime)== 5){
+
+      }
+    });
+
+    /// every 5 min check if the stations has changed
+    /// IF CHANGED CHANGE POINTS
+    /// get location of user and check
+    /// 
+  }
+
+  // changeStuff(){
+  //   timer = Timer.periodic(
+  //     const Duration(seconds: 10),
+  //     (_) => changeHello(), //(),
+  //   );
+  // }
+
+  // changeHello(){
+  //   hello = "hi"; 
+  //   print(hello);
+  // }
+}
