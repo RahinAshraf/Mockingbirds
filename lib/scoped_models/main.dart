0393922e58852138bdde4be99fe27fa21a4a07ff
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2022-03-08 11:19:56 +0000

    Update NavigationModel class in scoped_models.dart

--- lib/scoped_models/main.dart
+++ lib/scoped_models/main.dart
@@ -1,6 +1,69 @@
 import 'package:scoped_model/scoped_model.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+
+import '../models/docking_station.dart';
+import '../providers/docking_station_manager.dart';
 
 class NavigationModel extends Model {
+  String accessToken =
+      'pk.eyJ1IjoibW9ja2luZ2JpcmRzIiwiYSI6ImNrempyNnZtajNkbmkybm8xb3lybWE3MTIifQ.AsZJbQPNRb2N3unNdA98nQ';
+
+  late MapboxMapController? controller;
+  late CameraPosition _cameraPosition;
+  late MapboxMap map;
+  LatLng currentLatLng = const LatLng(51.51185004458236, -0.11580820118980878);
+  bool showMarkers = false; //for displaying markers with button
+
+  NavigationModel() {
+    _cameraPosition = CameraPosition(target: currentLatLng, zoom: 12);
+    createMap();
+  }
+
+  // @override
+  void _onMapCreated(MapboxMapController controller) async {
+    this.controller = controller;
+    fetchDockingStations();
+    // controller.onSymbolTapped.add(_onSymbolTapped);
+  }
+
+  void fetchDockingStations() {
+    final dockingStationManager _stationManager = dockingStationManager();
+    _stationManager
+        .importStations()
+        .then((value) => placeDockMarkers(_stationManager.stations));
+  }
+
+  void createMap() {
+    map = MapboxMap(
+      accessToken: accessToken,
+      initialCameraPosition: _cameraPosition,
+      onMapCreated: _onMapCreated,
+      // onStyleLoadedCallback: _onStyleLoadedCallback,
+      myLocationEnabled: true,
+      // myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+      annotationOrder: [AnnotationType.symbol],
+    );
+  }
+
+  void placeDockMarkers(List<DockingStation> docks) {
+    for (var station in docks) {
+      controller!.addSymbol(
+        SymbolOptions(
+            geometry: LatLng(station.lat, station.lon),
+            iconSize: 0.7,
+            iconImage: "assets/icon/bicycle.png"),
+      );
+    }
+  }
+
+  MapboxMap getMap() {
+    return map;
+  }
+
+  void setMap(MapboxMap map) {
+    this.map = map;
+  }
+
   String _name = "";
   int _count = 0;
 
