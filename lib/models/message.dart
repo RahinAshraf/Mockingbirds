7937b0528d94d919adadfffe4579c1afd89ac82e
Lilianna Szabo <szabo.lilianna@gmail.com>
2022-03-09 15:43:55 +0000

    HelpBot now can have multiple questions by topic

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 class Message {
   final String questionText;
   final String questionAnswer;
   final String topic;
   bool launch = false;
+  final String id;
 
-  Message(this.questionText, this.questionAnswer, this.topic);
+  Message(this.questionText, this.questionAnswer, this.topic, this.id);
 
 
   void setLaunch(bool launch){
