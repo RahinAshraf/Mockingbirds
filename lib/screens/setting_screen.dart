140b169dd59865a9ed78da4829546c5891cb4ac4
Tayyibah <k20059556@kcl.ac.uk>
2022-03-12 19:31:27 +0000

    Add basic options to settings page

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../styles/styling.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends StatelessWidget {
   @override
@@ -10,7 +11,26 @@ class Settings extends StatelessWidget {
         backgroundColor: appBarColor,
       ),
       body: Center(
-        child: Text("Settings page!"),
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: Text('Common'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: Icon(Icons.language),
+                  title: Text('Language'),
+                  value: Text('English'),
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {},
+                  initialValue: true,
+                  leading: Icon(Icons.format_paint),
+                  title: Text('Enable custom theme'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
