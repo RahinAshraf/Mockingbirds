6370cf6ae6fc8a98802bd35d5d09d98499953d55
Tayyibah <k20059556@kcl.ac.uk>
2022-03-13 13:37:59 +0000

    Refactor to create custom colours file

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'colors.dart';
 //use custom colours later on
 
 class CustomTheme with ChangeNotifier {
-  static Color appBarColor = Color(0xFF99D2A9);
-
   static bool _isDarkTheme = true;
   ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
 
   void toggleTheme() {
     _isDarkTheme = !_isDarkTheme;
-    print(_isDarkTheme);
-    print("here");
     notifyListeners();
   }
 
   static ThemeData get defaultTheme {
     return ThemeData(
-      scaffoldBackgroundColor: Color(0xffffffff),
-      appBarTheme: AppBarTheme(color: appBarColor),
-      primarySwatch: getDefaultMaterialColor(),
-      backgroundColor: Color(0xFF99D2A9),
+      scaffoldBackgroundColor: CustomColors.lightModeScaffoldColour,
+      appBarTheme: AppBarTheme(color: CustomColors.green),
+      primarySwatch: CustomColors.lightGreen,
+      backgroundColor: CustomColors.green,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: Color(0xFF99D2A9),
+        backgroundColor: CustomColors.green,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey[10],
       ),
@@ -33,7 +29,7 @@ class CustomTheme with ChangeNotifier {
     return ThemeData(
       scaffoldBackgroundColor: Colors.grey[900],
       appBarTheme: AppBarTheme(color: Colors.grey[900]),
-      primarySwatch: getDarkMaterialColor(),
+      primarySwatch: CustomColors.lightGreen,
       backgroundColor: Colors.grey[900],
       textTheme: ThemeData.dark().textTheme,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
@@ -41,42 +37,6 @@ class CustomTheme with ChangeNotifier {
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey[10],
       ),
-    );
-  }
-
-  static MaterialColor getDefaultMaterialColor() {
-    return MaterialColor(
-      0xff99d2a9, // 0%
-      const <int, Color>{
-        50: const Color(0xffa3d7b2), //10%
-        100: const Color(0xffaddbba), //20%
-        200: const Color(0xffb8e0c3), //30%
-        300: const Color(0xffc2e4cb), //40%
-        400: const Color(0xffcce9d4), //50%
-        500: const Color(0xffd6eddd), //60%
-        600: const Color(0xffe0f2e5), //70%
-        700: const Color(0xffebf6ee), //80%
-        800: const Color(0xfff5fbf6), //90%
-        900: const Color(0xffffff), //100%
-      },
-    );
-  }
-
-  static MaterialColor getDarkMaterialColor() {
-    return const MaterialColor(
-      0xff99d2a9, // 0%
-      <int, Color>{
-        50: Color(0xffa3d7b2), //10%
-        100: Color(0xffaddbba), //20%
-        200: Color(0xffb8e0c3), //30%
-        300: Color(0xffc2e4cb), //40%
-        400: Color(0xffcce9d4), //50%
-        500: Color(0xffd6eddd), //60%
-        600: Color(0xffe0f2e5), //70%
-        700: Color(0xffebf6ee), //80%
-        800: Color(0xfff5fbf6), //90%
-        900: Color(0xffffff), //100%
-      },
     );
   }
 }
