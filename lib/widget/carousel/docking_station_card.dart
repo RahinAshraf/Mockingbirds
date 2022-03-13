6ec71cbbfa28200fe0158964e7210b00b8e0740f
Hristina-Andreea Sararu <hristina-andreea.sararu@kcl.ac.uk>
2022-03-13 00:12:54 +0000

    display information card about a docking station after it is clicked

--- lib/widget/carousel/docking_station_card.dart
+++ lib/widget/carousel/docking_station_card.dart
@@ -2,41 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:favorite_button/favorite_button.dart';
 
 ///Creates a card for a docking station, to include its name, number of bikes and empty bikes.
+import 'package:flutter/material.dart';
+import 'package:favorite_button/favorite_button.dart';
+
+///Creates a card for a docking station, to include its name, number of bikes and empty bikes.
 
 Widget dockingStationCard(
     int index, String name, String nb_bikes, String nb_empty_docks) {
   return Card(
-    elevation: 1.0,
-    shadowColor: Colors.green[200],
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-    clipBehavior: Clip.antiAlias,
-    child: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          FavoriteButton(valueChanged: (_isFavorite) {}),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Divider(
-                  color: Colors.black,
-                ),
-                Text('Total number of bikes: $nb_bikes'),
-                Text('Available bikes: $nb_empty_docks'),
-              ],
+      elevation: 1.0,
+      shadowColor: Colors.green[200],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      clipBehavior: Clip.antiAlias,
+      child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            FavoriteButton(valueChanged: (_isFavorite) {}),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Image.asset('assets/images/logo.png')),
+                  Expanded(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        name,
+                        style: const TextStyle(
+                          color: Color(0xFF99D2A9),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                      Text('Total number of bikes: $nb_bikes'),
+                      Text('Available bikes: $nb_empty_docks'),
+                    ],
+                  ))
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    ),
-  );
+          ])));
 }
