0c6557c044dcefcef36b833ff897cd52bb194a2f
Marija Buivyte <k20082541@kcl.ac.uk>
2022-03-17 01:12:39 +0000

    Clean some code

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -5,78 +5,67 @@ import '../screens/favourite_screen.dart';
 import '../screens/setting_screen.dart';
 import '../screens/help_screen.dart';
 
-///Author: Tayyibah
-
+/// Author: Tayyibah
 class NavigationDrawerWidget extends StatelessWidget {
-  final padding = EdgeInsets.symmetric(horizontal: 20);
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(height: 48),
-            buildMenuItem(
-              text: 'Schedule',
-              icon: Icons.date_range,
-              onClicked: () => selectedItem(context, 0),
-            ),
-            buildMenuItem(
-              text: 'My Journeys',
-              icon: Icons.history,
-              onClicked: () => selectedItem(context, 1),
-            ),
-            buildMenuItem(
-              text: 'Favourites',
-              icon: Icons.favorite,
-              onClicked: () => selectedItem(context, 2),
-            ),
-            buildMenuItem(
-              text: 'Statistics',
-              icon: Icons.assessment_outlined,
-              onClicked: () => selectedItem(context, 3),
-            ),
-            //decided to remove this, not sure if it looks nice
-            const SizedBox(height: 24),
-            Divider(color: Colors.grey),
-            const SizedBox(height: 24),
-            buildMenuItem(
-              text: 'Help',
-              icon: Icons.chat_bubble_outlined,
-              onClicked: () => selectedItem(context, 4),
-            ),
-            buildMenuItem(
-              text: 'Settings',
-              icon: Icons.settings,
-              onClicked: () => selectedItem(context, 5),
-            ),
-            buildMenuItem(
-              text: 'Log Out',
-              icon: Icons.logout,
-              onClicked: () => FirebaseAuth.instance.signOut(),
-            ),
-          ],
-        ),
+      child: ListView(
+        children: <Widget>[
+          const SizedBox(height: 48),
+          buildMenuItem(
+            text: 'Schedule',
+            icon: Icons.date_range,
+            onClicked: () => selectedItem(context, 0),
+          ),
+          buildMenuItem(
+            text: 'My Journeys',
+            icon: Icons.history,
+            onClicked: () => selectedItem(context, 1),
+          ),
+          buildMenuItem(
+            text: 'Favourites',
+            icon: Icons.favorite,
+            onClicked: () => selectedItem(context, 2),
+          ),
+          buildMenuItem(
+            text: 'Statistics',
+            icon: Icons.assessment_outlined,
+            onClicked: () => selectedItem(context, 3),
+          ),
+          const SizedBox(height: 24),
+          const Divider(color: Colors.grey),
+          const SizedBox(height: 24),
+          buildMenuItem(
+            text: 'Help',
+            icon: Icons.chat_bubble_outlined,
+            onClicked: () => selectedItem(context, 4),
+          ),
+          buildMenuItem(
+            text: 'Settings',
+            icon: Icons.settings,
+            onClicked: () => selectedItem(context, 5),
+          ),
+          buildMenuItem(
+            text: 'Log Out',
+            icon: Icons.logout,
+            onClicked: () => FirebaseAuth.instance.signOut(),
+          ),
+        ],
       ),
     );
   }
 
-  Widget buildMenuItem({
-    required String text,
-    required IconData icon,
-    VoidCallback? onClicked,
-  }) {
-    final color = Colors.green;
-
+  Widget buildMenuItem(
+      {required String text, required IconData icon, VoidCallback? onClicked}) {
     return ListTile(
       leading: Icon(icon),
-      title: Text(text, style: TextStyle(color: color)),
+      title: Text(text, style: const TextStyle(color: Colors.green)),
       onTap: onClicked,
     );
   }
 
   selectedItem(BuildContext context, int i) {
-    //Navigator.of(context).pop();
     switch (i) {
       case 0:
         Navigator.of(context).push(MaterialPageRoute(
@@ -85,7 +74,8 @@ class NavigationDrawerWidget extends StatelessWidget {
         break;
       case 1:
         Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => Placeholder(),
+          builder: (context) => const Placeholder(),
+          // TODO: link 'My journeys' screen here
         ));
         break;
       case 2:
@@ -95,12 +85,8 @@ class NavigationDrawerWidget extends StatelessWidget {
         break;
       case 3:
         Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => Placeholder(),
-        ));
-        break;
-      case 3:
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => Placeholder(),
+          builder: (context) => const Placeholder(),
+          // TODO: link 'Statistics' screen here
         ));
         break;
       case 4:
