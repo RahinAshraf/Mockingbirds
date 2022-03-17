05a38e84e5072bbe6a708241204699f17205fd02
Marija Buivyte <k20082541@kcl.ac.uk>
2022-03-17 18:46:26 +0000

    Refactor helpbot

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -4,11 +4,15 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:veloplan/models/message.dart';
 import 'package:veloplan/styles/styling.dart';
 import 'package:veloplan/utilities/help_bot_manager.dart';
+import 'package:veloplan/widgets/helpbot_topics_list_widget.dart';
 import 'package:veloplan/widgets/message_bubble_widget.dart';
 
-const url =
+const String url =
     'mailto:k20070238@kcl.ac.uk?subject=Help%20with%20app&body=Help%20me!';
 HelpBotManager questions = HelpBotManager();
+
+// STYLING
+const Color helpScreenBorderColor = Color(0x4D99D2A9);
 
 class HelpPage extends StatefulWidget {
   @override
@@ -19,8 +23,8 @@ class _HelpPageState extends State<HelpPage> {
   List<MessageBubble> _conversation = [
     MessageBubble(content: 'Hello. How can I help you?')
   ];
+  List<ChoiceButton> choices = [];
   String selectedTopic = "";
-  List<TopicsList> choices = [];
 
   @override
   void initState() {
@@ -49,11 +53,12 @@ class _HelpPageState extends State<HelpPage> {
               decoration: const BoxDecoration(
                 border: Border(
                   top: BorderSide(
-                    color: Color(0x4D99D2A9),
+                    color: helpScreenBorderColor,
+                    width: 1.5,
                   ),
                 ),
               ),
-              padding: const EdgeInsets.only(bottom: 10.0, top: 10.0),
+              padding: const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
@@ -76,10 +81,10 @@ class _HelpPageState extends State<HelpPage> {
     }
   }
 
-  List<TopicsList> _displayTopics() {
-    List<TopicsList> tpcs = [];
+  List<ChoiceButton> _displayTopics() {
+    List<ChoiceButton> topics = [];
     for (String topic in questions.getAllTopics()) {
-      tpcs.add(TopicsList(
+      topics.add(ChoiceButton(
         topic: topic,
         onPressed: () {
           setState(() {
@@ -90,59 +95,29 @@ class _HelpPageState extends State<HelpPage> {
         },
       ));
     }
-    return tpcs;
+    return topics;
   }
 
-  List<TopicsList> _displayQuestions() {
-    List<TopicsList> tpcs = [];
+  void _displayQuestions() {
     for (Message message in questions.getMessagesByTopic(selectedTopic)) {
-      choices.add(TopicsList(
-        topic: questions.getQuestionText(message),
-        onPressed: () {
-          setState(() {
-            _conversation.add(MessageBubble(
-                content: questions.getQuestionText(message),
-                isSentByBot: false));
-            _conversation.add(
-                MessageBubble(content: questions.getQuestionAnswer(message)));
-            choices = _displayTopics();
-            if (questions.getLaunch(message)) {
-              _sendMail();
-            }
-          });
-        },
-      ));
+      choices.add(
+        ChoiceButton(
+          topic: questions.getQuestionText(message),
+          onPressed: () {
+            setState(() {
+              _conversation.add(MessageBubble(
+                  content: questions.getQuestionText(message),
+                  isSentByBot: false));
+              _conversation.add(
+                  MessageBubble(content: questions.getQuestionAnswer(message)));
+              choices = _displayTopics();
+              if (questions.getLaunch(message)) {
+                _sendMail();
+              }
+            });
+          },
+        ),
+      );
     }
-    return tpcs;
-  }
-}
-
-class TopicsList extends StatelessWidget {
-  const TopicsList({required this.topic, required this.onPressed});
-  final VoidCallback onPressed;
-  final String topic;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 5.0),
-      child: OutlinedButton(
-          style: ButtonStyle(
-            overlayColor:
-                MaterialStateProperty.all<Color>(const Color(0x1A99D2A9)),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-            ),
-          ),
-          onPressed: onPressed,
-          child: Text(
-            topic,
-            style: const TextStyle(
-              color: Color(0xFF99D2A9),
-            ),
-          )),
-    );
   }
 }
