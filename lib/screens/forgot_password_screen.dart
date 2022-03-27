b371a39c2e4685965095b91c4beca06bd3977b91
Hristina-Andreea Sararu <hristina-andreea.sararu@kcl.ac.uk>
2022-03-27 17:46:44 +0100

    adjust forget password screen layout

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -38,25 +38,50 @@ class ForgotPasswordScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          TextFormField(
-            key: ValueKey('email'),
-            controller: emailController,
-            autocorrect: false,
-            textCapitalization: TextCapitalization.none,
-            enableSuggestions: false,
-            validator: (value) => EmailValidator.validate(value!)
-                ? null
-                : "Please enter a valid email",
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email Address',
-            ),
-          ),
-          ElevatedButton(
-            child: Text('Send password reset email'),
-            onPressed: () => sendPasswordResetEmail(emailController.text, context),
-          ),
+          const SizedBox(height: 35),
+          Container(
+              height: 170.0,
+              width: 170.0,
+              alignment: Alignment.topRight,
+              child: Image.asset('assets/images/forgot_password_icon.png')),
+          Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Please enter your email address to  request a password reset:',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 25),
+                    TextFormField(
+                      key: ValueKey('email'),
+                      controller: emailController,
+                      autocorrect: false,
+                      textCapitalization: TextCapitalization.none,
+                      enableSuggestions: false,
+                      validator: (value) => EmailValidator.validate(value!)
+                          ? null
+                          : "Please enter a valid email",
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xFF99D2A9), width: 2.0),
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: 'Email Address',
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    ElevatedButton(
+                      child: Text('Send password reset email'),
+                      onPressed: () =>
+                          sendPasswordResetEmail(emailController.text, context),
+                    ),
+                  ])),
         ],
       ),
     );
