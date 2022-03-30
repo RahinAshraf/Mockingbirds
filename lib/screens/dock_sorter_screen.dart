26308b3bc4abefcbad5e0dc187e0be98bd43b908
Marija Buivyte <k20082541@kcl.ac.uk>
2022-03-30 12:42:56 +0100

    Refactor dock sorter and trip scheduler screens.

--- lib/screens/dock_sorter_screen.dart
+++ lib/screens/dock_sorter_screen.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-import 'package:veloplan/.env.dart';
-import 'package:veloplan/helpers/shared_prefs.dart';
+import 'package:veloplan/models/map_models/base_map_model.dart';
+import 'package:veloplan/scoped_models/map_model.dart';
 import 'package:veloplan/widgets/docking_stations_sorting_widget.dart';
 
-/// The edit dock screen which is useful for selecting and favouriting docking station cards
-/// Author(s): Marija, Nicole Lehchevska
-
+/// Edit dock screen displaying map with [DockSorter] panel.
 class DockSorterScreen extends StatefulWidget {
   late final LatLng userCoord;
   DockSorterScreen(this.userCoord, {Key? key}) : super(key: key);
-
   @override
   _DockSorterScreen createState() => _DockSorterScreen();
 }
 
 class _DockSorterScreen extends State<DockSorterScreen> {
-  late CameraPosition _initialCameraPosition;
-  LatLng latLng = getLatLngFromSharedPrefs();
-  late MapboxMapController controller;
   final panelController = PanelController();
   late LatLng userCoordinates;
+  late BaseMapboxMap _baseMap;
 
   @override
   void initState() {
     userCoordinates = super.widget.userCoord;
     super.initState();
-    _initialCameraPosition = CameraPosition(target: latLng, zoom: 16);
   }
-
-  _onMapCreated(MapboxMapController controller) async {
-    this.controller = controller;
-  }
-  // TODO: Marija refactor to use base map, example in screens/navigation/map_screen.dart
 
   @override
   Widget build(BuildContext context) {
@@ -46,23 +36,11 @@ class _DockSorterScreen extends State<DockSorterScreen> {
         maxHeight: panelHeightOpen,
         borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
         controller: panelController,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: MapboxMap(
-                  accessToken: MAPBOX_ACCESS_TOKEN,
-                  initialCameraPosition: _initialCameraPosition,
-                  onMapCreated: _onMapCreated,
-                  myLocationEnabled: true,
-                  myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: ScopedModelDescendant<MapModel>(
+            builder: (BuildContext context, Widget? child, MapModel model) {
+          _baseMap = BaseMapboxMap(model);
+          return Stack(children: _baseMap.getWidgets());
+        }),
         panelBuilder: (controller) =>
             DockSorter(userCoordinates, controller: controller),
       ),
