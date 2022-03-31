1392cb1e5685d16df8ca5d9f141d391a31712343
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2022-03-31 13:54:58 +0100

    Add tests for login

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,98 +1,21 @@
-import 'dart:io';
-
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
-import 'dart:io';
-import 'package:path/path.dart';
+import 'envVars.dart';
 
 void main() {
-  group('first', () {
+  group('log in', () {
     FlutterDriver? driver;
     final emailField = find.byValueKey("email");
     final passwordField = find.byValueKey("password");
     final loginButton = find.byValueKey('logIn');
     final snackbar = find.byType("SnackBar");
     final navBarPage = find.byType("NavBar");
+    final emailErrorMessage = find.byValueKey("email");
+    final passwordErrorMessage = find.byValueKey("password");
 // connect flutter driver to the app before executing the runs
     setUpAll(() async {
-      print("----------" + Platform.environment.toString());
-      final envVars = Platform.environment;
-      print("----------" + envVars['ANDROID_SDK_ROOT'].toString());
-      final adbPath = join(
-        envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
-        'platform-tools',
-        Platform.isWindows ? 'adb.exe' : 'adb',
-      );
-      await Process.run(adbPath, [
-        'shell',
-        'pm',
-        'grant',
-        'com.example.yourapp', // replace with your app id
-        'android.permission.ACCESS_FINE_LOCATION'
-      ]);
       driver = await FlutterDriver.connect();
-
-      // final Map<String, String> envVars = Platform.environment;
-      // final String adbPath =
-      //     envVars['ANDROID_SDK_ROOT']! + '/platform-tools/adb.exe';
-      // await Process.run(adbPath, [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.READ_EXTERNAL_STORAGE'
-      // ]);
-      // await Process.run(adbPath, [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.READ_PHONE_STATE'
-      // ]);
-      // await Process.run(adbPath, [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.ACCESS_FINE_LOCATION'
-      // ]);
-      // driver = await FlutterDriver.connect();
-      // driver = await FlutterDriver.connect(timeout: Duration(seconds: 5));
-      // await Process.run('add_adb_path/adb.exe', [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.ACCESS_MEDIA_LOCATION'
-      // ]);
-      // await Process.run('add_adb_path/adb.exe', [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.ACCESS_COARSE_LOCATION'
-      // ]);
-      // await Process.run('add_adb_path/adb.exe', [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.ACCESS_BACKGROUND_LOCATION'
-      // ]);
-      // await Process.run('add_adb_path/adb.exe', [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.READ_EXTERNAL_STORAGE'
-      // ]);
-      // await Process.run('add_adb_path/adb.exe', [
-      //   'shell',
-      //   'pm',
-      //   'grant',
-      //   'com.example.veloplan',
-      //   'android.permission.WRITE_EXTERNAL_STORAGE'
-      // ]);
+      await setUpEnvVars();
     });
 // disconnect flutter driver from the app after executing the runs
     tearDownAll(() async {
@@ -100,20 +23,55 @@ void main() {
         driver?.close();
       }
     });
-
-    test("login failed", () async {
+    test("test incorrect password but correct email", () async {
       await driver?.tap(emailField);
       await driver?.enterText("elisabeth.koren.halvorsen@gmail.com");
-//       await driver?.enterText("elisabeth.koren.halvorsen@gmail.com");
+      await driver?.tap(passwordField);
+      await driver?.enterText("Password1234");
+      await driver?.tap(loginButton);
+      await driver?.waitFor(snackbar);
+      assert(snackbar != null);
+      await driver?.waitUntilNoTransientCallbacks();
+      assert(navBarPage == null);
+    });
+
+    test("test correct password but incorrect email", () async {
+      await driver?.waitForAbsent(snackbar);
+      await driver?.tap(emailField);
+      await driver?.enterText("elisabeth.koren.halvor@gmail.com");
       await driver?.tap(passwordField);
       await driver?.enterText("Password123");
-      // await Future.delayed(Duration(seconds: 3));
       await driver?.tap(loginButton);
-      // await driver?.waitFor(snackbar);
-      // assert(snackbar != null);
+      await driver?.waitFor(snackbar);
+      assert(snackbar != null);
+      await driver?.waitUntilNoTransientCallbacks();
+      assert(navBarPage == null);
+    });
+
+    test("test no fields filled in", () async {
+      await driver?.waitForAbsent(snackbar);
+      await driver?.tap(emailField);
+      await driver?.enterText("");
+      await driver?.tap(passwordField);
+      await driver?.enterText("");
+      await driver?.tap(loginButton);
+      assert(snackbar == null);
+      assert(passwordErrorMessage != null);
+      assert(emailErrorMessage != null);
+      await driver?.waitUntilNoTransientCallbacks();
+      assert(navBarPage == null);
+    });
+
+    test("login succeeded", () async {
+      await driver?.waitForAbsent(snackbar);
+      await driver?.tap(emailField);
+      await driver?.enterText("elisabeth.koren.halvorsen@gmail.com");
+      await driver?.tap(passwordField);
+      await driver?.enterText("Password123");
+      await driver?.tap(loginButton);
       assert(snackbar == null);
       await driver?.waitUntilNoTransientCallbacks();
-      assert(navBarPage != null);
+      assert(navBarPage == null);
     });
   });
 }
